4c6bcacef30e5be6df4050a30d8c0c05cfe30be9
Bryan Mytko <bryanmytko@gmail.com>
2015-12-30 21:12:05 -0500

    Add generic 404 response handling

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,9 @@
 module Api::V1
   class ApiController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    def not_found
+      render :json => { errors: "Not found." }, status: 404
+    end
   end
 end
