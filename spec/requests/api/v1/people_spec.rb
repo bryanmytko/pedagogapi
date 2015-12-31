1a7d5637c14d9448747518c0d7594969d10ebc0f
Bryan Mytko <bryanmytko@gmail.com>
2015-12-30 21:12:52 -0500

    Update people request spec to include invalid responses

--- spec/requests/api/v1/people_spec.rb
+++ spec/requests/api/v1/people_spec.rb
@@ -8,4 +8,24 @@ describe "People", type: :request do
       expect(response.status).to eq(200)
     end
   end
+
+  describe "GET /api/v1/people/:id" do
+    let(:person) { FactoryGirl.create(:person) }
+
+    context "when the request is valid" do
+      before { get "/api/v1/people/#{person.id}", { format: :json } }
+
+      it "responds with OK status" do
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context "when the request is invalid" do
+      before { get "/api/v1/people/9000+", { format: :json } }
+
+      it "responds with ERROR status" do
+        expect(response.status).to eq(404)
+      end
+    end
+  end
 end
