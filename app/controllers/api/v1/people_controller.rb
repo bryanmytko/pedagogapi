e79fa4b3eafa1dd07f9f937f893466da9a345635
Bryan Mytko <bryanmytko@gmail.com>
2015-12-30 21:12:16 -0500

    Add people#show

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,6 +1,17 @@
 module Api::V1
   class PeopleController < ApiController
     def index
+      @people = Person.all
+    end
+
+    def show
+      @person = Person.find(person_params[:id])
+    end
+
+    private
+
+    def person_params
+      params.permit(:id)
     end
   end
 end
