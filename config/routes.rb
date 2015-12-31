24cd183b1e56f948c760d7643b7484dd63ff702d
Bryan Mytko <bryanmytko@gmail.com>
2015-12-31 02:48:04 -0500

    Add Superheroes route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope module: :api, defaults: { format: :json } do
     scope :api do
       namespace :v1 do
-        resources :people, only: [:index, :show]
+        resources :superheroes, only: [:index, :show]
       end
     end
   end
