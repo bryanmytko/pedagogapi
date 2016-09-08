bf178f95abf2e5a9f9385a352f0fbb595137c802
Bryan Mytko <bryanmytko@gmail.com>
2016-09-08 16:39:56 -0400

    Update API specs to use rspec / rails 5 params key/value

--- spec/controllers/api/v1/superheroes_controller_spec.rb
+++ spec/controllers/api/v1/superheroes_controller_spec.rb
@@ -5,7 +5,7 @@ describe Api::V1::SuperheroesController, type: :controller do
 
   describe "GET #index" do
     it "returns all superheroes" do
-      get :index, format: :json
+      get :index, params: { format: :json }
       expect(response.status).to eq 200
     end
   end
@@ -13,14 +13,14 @@ describe Api::V1::SuperheroesController, type: :controller do
   describe "GET #show" do
     context "valid request" do
       it "returns the superhero" do
-        get :show, format: :json, params: { id: superhero.id }
+        get :show, params: { id: superhero.id, format: :json }
         expect(response.status).to eq 200
       end
     end
 
     context "invalid request" do
       it "returns nil" do
-        get :show, format: :json, params: { id: -1 }
+        get :show, params: { id: -1, format: :json}
         expect(response.status).to eq 404
       end
     end
--- spec/requests/api/v1/superheroes_spec.rb
+++ spec/requests/api/v1/superheroes_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Superheroes", type: :request do
   describe "GET /api/v1/superheroes/" do
-    before { get "/api/v1/superheroes", { format: :json } }
+    before { get "/api/v1/superheroes", params: { format: :json } }
 
     it "responds with OK status" do
       expect(response.status).to eq(200)
@@ -13,7 +13,7 @@ describe "Superheroes", type: :request do
     let(:superhero) { FactoryGirl.create(:superhero) }
 
     context "when the request is valid" do
-      before { get "/api/v1/superheroes/#{superhero.id}", { format: :json } }
+      before { get "/api/v1/superheroes/#{superhero.id}", params: { format: :json } }
 
       it "responds with OK status" do
         expect(response.status).to eq(200)
@@ -21,7 +21,7 @@ describe "Superheroes", type: :request do
     end
 
     context "when the request is invalid" do
-      before { get "/api/v1/superheroes/9000+", { format: :json } }
+      before { get "/api/v1/superheroes/9000+", params: { format: :json } }
 
       it "responds with ERROR status" do
         expect(response.status).to eq(404)
